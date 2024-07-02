e7b481bafca3be03b284c1d7d658bade128a349f
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-02 20:40:13 +0530

    DP-19 - 0/1 Knapsack using 3 approaches

---
+++ 2D DP/0-1Knapsack.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+// * Recursive Solution
+int f(int idx, int weight, vector<int> wt, vector<int> value) {
+	if(idx == 0) {
+		if(wt[idx] <= weight)
+			return value[idx];
+		return 0;
+	}
+
+	int take = 0, notTake = 0;
+
+	if(wt[idx] <= weight)
+		take = value[idx] + f(idx - 1, weight - wt[idx], wt, value);
+	
+	notTake = f(idx - 1, weight, wt, value);
+
+	return max(take, notTake);
+}
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	return f(n - 1, maxWeight, weight, value);
+}
+
+
+// * Memoization Solution
+int f(int idx, int weight, vector<int> wt, vector<int> value, vector<vector<int>> &dp) {
+	if(idx == 0) {
+		if(wt[idx] <= weight)
+			return value[idx];
+		return 0;
+	}
+	
+	if(dp[idx][weight] != -1)
+		return dp[idx][weight];
+
+	int take = -1e9, notTake = -1e9;
+
+	if(wt[idx] <= weight)
+		take = value[idx] + f(idx - 1, weight - wt[idx], wt, value, dp);
+	
+	notTake = f(idx - 1, weight, wt, value, dp);
+
+	return dp[idx][weight] = max(take, notTake);
+}
+
+
+//  *Tabulation Solution
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	vector<vector<int>> dp(n, vector<int>(maxWeight+1, 0));
+	
+	for(int W = weight[0]; W <= maxWeight; W++)
+		dp[0][W] = value[0];
+
+  // * 1. Changing States: Idx and weight
+	for(int i = 1; i < n; i++) {
+		for(int W = 0; W <= maxWeight; W++) {
+			// * Copy the recurrence as it is.
+			int take = -1e9, notTake = 0;
+			if(weight[i] <= W)
+				take = value[i] + dp[i - 1][W - weight[i]];
+			notTake = 0 + dp[i - 1][W];
+			dp[i][W] = max(take, notTake);
+		}
+	}
+	return dp[n-1][maxWeight];
+}
