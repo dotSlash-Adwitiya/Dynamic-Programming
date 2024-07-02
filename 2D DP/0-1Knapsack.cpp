01624e20649215038fe34f3e1e11fcc46ecc18ad
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-02 20:44:32 +0530

    DP-19 - 0/1 Knapsack using 4 approaches (added SPACE OPTIMIZATION)

--- 2D DP/0-1Knapsack.cpp
+++ 2D DP/0-1Knapsack.cpp
@@ -68,3 +68,28 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 	}
 	return dp[n-1][maxWeight];
 }
+
+// * SPACE OPTIMISED Solution, because we only need prevRow (idx - 1);
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	vector<vector<int>> dp(n, vector<int>(maxWeight+1, 0));
+	vector<int> prev(maxWeight+1, 0), curr(maxWeight+1, 0);
+
+	for(int W = weight[0]; W <= maxWeight; W++)
+		prev[W] = value[0];
+
+	for(int i = 1; i < n; i++) {
+		for(int W = 0; W <= maxWeight; W++) {
+			
+			int take = -1e9, notTake = 0;
+			if(weight[i] <= W)
+				take = value[i] + prev[W - weight[i]];
+			notTake = 0 + prev[W];
+			curr[W] = max(take, notTake);
+		}
+		prev = curr;
+	}
+
+
+	return prev[maxWeight];
+}
