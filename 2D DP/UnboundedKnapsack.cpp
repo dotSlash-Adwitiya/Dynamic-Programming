79f242f6fc5ff9cf9a7b97672551a22b2d18a06c
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-05 23:37:24 +0530

    Unbounded Knapsack (Infinite item supply) 4 Approaches

---
+++ 2D DP/UnboundedKnapsack.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// * Recursively NAIVE Solution :
+int fNaive(int idx, int target, vector<int> &profit, vector<int> &weight) {
+
+  if(idx == 0){
+      if(weight[idx] <= target)
+          profit[idx] + fNaive(idx, target - weight[idx], profit, weight); // * Make recursive calls for this idx
+      return 0;
+  }
+  
+  int pick = 0;
+  if(weight[idx] <= target)
+      pick = profit[idx] + fNaive(idx, target - weight[idx], profit, weight);
+  int notPick = fNaive(idx - 1, target, profit, weight);
+
+  return max(pick , notPick);
+}
+
+int unboundedKnapsack(int n, int w, vector<int> &profit, vector<int> &weight){
+  return fNaive(n-1, w, profit, weight);
+}
+
+// * Recursive Solution
+int f(int idx, int target, vector<int> &profit, vector<int> &weight) {
+
+  if(idx == 0){
+      if(weight[0] <= target)
+          return (target / weight[0]) * profit[0];
+      return 0;
+  }
+  
+  int pick = 0;
+  if(weight[idx] <= target)
+      pick = profit[idx] + f(idx, target - weight[idx], profit, weight);
+  int notPick = f(idx - 1, target, profit, weight);
+
+  return max(pick , notPick);
+}
+
+int unboundedKnapsack(int n, int w, vector<int> &profit, vector<int> &weight){
+  return f(n-1, w, profit, weight);
+}
+
+// * Memoization Solution
+int f(int idx, int target, vector<int> &profit, vector<int> &weight, vector<vector<int>> &dp) {
+  if(idx == 0){
+      if(weight[0] <= target)
+          return (target / weight[0]) * profit[0];      
+      return 0;
+  }
+  if(dp[idx][target] != -1)
+      return dp[idx][target];
+  
+  int pick = 0;
+  if(weight[idx] <= target)
+      pick = profit[idx] + f(idx, target - weight[idx], profit, weight, dp);
+  int notPick = f(idx - 1, target, profit, weight, dp);
+
+  return dp[idx][target] = max(pick , notPick);
+}
+
+int unboundedKnapsack(int n, int w, vector<int> &profit, vector<int> &weight){
+  vector<vector<int>> dp(n, vector<int>(w+1, -1));
+  return f(n-1, w, profit, weight, dp);
+}
+
+//  * Tabulation Solution
+int unboundedKnapsack(int n, int w, vector<int> &profit, vector<int> &weight){
+  vector<vector<int>> dp(n, vector<int>(w+1, -1));
+
+  //  * For weight[] = {2,4,6}, the range of weights can be in : 2,3,4,5,6
+  // * The DP[][weight[idx]] created will be able to store: 0,1,2,3,4,5,6 
+  // * From recursive base case, we get: For every 0th index, of weights,
+  for(int target = 0; target <= w; target++)
+      dp[0][target] = target / weight[0] * profit[0];
+
+  for(int idx = 1; idx < n; idx++) {
+      for(int target = 0; target <= w; target++){
+          int pick = 0;
+          if(weight[idx] <= target)
+              pick = profit[idx] + dp[idx][target - weight[idx]];
+          int notPick = dp[idx - 1][target];
+
+          dp[idx][target] = max(pick , notPick);
+      }
+  }
+  return dp[n-1][w];
+}
+
+// * Tabulation with Two 1-D Arrays :
+int unboundedKnapsack(int n, int w, vector<int> &profit, vector<int> &weight){
+  vector<int> curr(w+1, 0), prev(w+1, 0);
+
+  for(int target = 0; target <= w; target++)
+      prev[target] = target / weight[0] * profit[0];
+
+  for(int idx = 1; idx < n; idx++) {
+      for(int target = 0; target <= w; target++){
+          int pick = 0;
+          if(weight[idx] <= target)
+              pick = profit[idx] + curr[target - weight[idx]];
+          int notPick = prev[target];
+
+          curr[target] = max(pick , notPick);
+      }
+      prev = curr;
+  }
+  return prev[w];
+}
