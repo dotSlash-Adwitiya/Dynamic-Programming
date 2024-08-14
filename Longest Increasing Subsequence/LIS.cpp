739d8041c5de2f9d4ec1f9b22c1252dd33cf59ed
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-08-14 23:21:21 +0530

    Added Binary Search Approach

--- Longest Increasing Subsequence/LIS.cpp
+++ Longest Increasing Subsequence/LIS.cpp
@@ -78,3 +78,20 @@ int longestIncreasingSubsequence(int arr[], int n) {
 
   return next[0];
 }
+
+// * BINARY SEARCH APPROACH
+int longestIncreasingSubsequence(int arr[], int n) {
+    vector<int> temp;
+    temp.push_back(arr[0]);
+
+    for (int i = 1; i < n; i++) {
+       if(arr[i] > temp.back()) 
+            temp.push_back(arr[i]);
+        else{
+            int idx = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[idx] = arr[i];
+        }
+    }
+
+    return temp.size();
+}
