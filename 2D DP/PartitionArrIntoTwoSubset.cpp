7cd7d50a310a3468b19af0d73ae34183db2f579d
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-08-28 23:39:49 +0530

    Tabulation Approach Added

--- 2D DP/PartitionArrIntoTwoSubset.cpp
+++ 2D DP/PartitionArrIntoTwoSubset.cpp
@@ -73,3 +73,36 @@ bool canPartition(vector<int> &arr, int n)
 
 	return subsetSumToK(n, totSum/2, arr);
 }
+
+// * Tabulation Appraoch
+ int equalPartition(int N, int arr[]) {
+    int sum = 0;
+    for(int i = 0; i < N; i++)
+        sum += arr[i];
+        
+    if(sum % 2)
+        return 0;
+    int target = sum / 2;
+    vector<vector<bool>> dp(N + 1, vector<bool>(target + 1, false));
+
+    for (int idx = 0; idx <= N; idx++) {
+        for (int tar = 0; tar <= target; tar++) {
+            if (idx == 0)
+                dp[idx][tar] = false;
+            if (tar == 0)
+                dp[idx][tar] = true;
+        }
+    }
+
+    for (int idx = 1; idx <= N; idx++) {
+        for (int tar = 1; tar <= target; tar++) {
+            if (arr[idx - 1] <= tar) {
+                dp[idx][tar] = dp[idx - 1][tar - arr[idx - 1]] || dp[idx - 1][tar];
+            } else {
+                dp[idx][tar] = dp[idx - 1][tar];
+            }
+        }
+    }
+
+    return dp[N][target];
+}
