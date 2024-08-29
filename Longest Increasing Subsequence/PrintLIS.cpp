fb9ffb8f4e6f21b286dc075c0814f219dfe70a79
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-08-29 23:04:07 +0530

    DP.42 Using Tabulation - LIS single Array approach

--- Longest Increasing Subsequence/PrintLIS.cpp
+++ Longest Increasing Subsequence/PrintLIS.cpp
@@ -5,19 +5,20 @@ using namespace std;
 vector<int> printingLongestIncreasingSubsequence(vector<int> arr, int n) {
     vector<int> dp(n, 1), hash(n);
     int lastIdx = 0, maxLen = 1;
+
     for(int idx = 0; idx < n; idx++){
-    hash[idx] = idx;
+      hash[idx] = idx;
     
-    for(int prevIdx = 0; prevIdx <= idx-1; prevIdx++){
-      if(arr[prevIdx] < arr[idx] && 1 + dp[prevIdx] > dp[idx]){
-          dp[idx] = 1 + dp[prevIdx];
-          hash[idx] = prevIdx;
+      for(int prevIdx = 0; prevIdx <= idx-1; prevIdx++){
+        if(arr[prevIdx] < arr[idx] && 1 + dp[prevIdx] > dp[idx]){
+            dp[idx] = 1 + dp[prevIdx];
+            hash[idx] = prevIdx;
+        }
       }
-    }
-    if(dp[idx] > maxLen){
-        maxLen = dp[idx];
-        lastIdx = idx;
-    }
+      if(dp[idx] > maxLen){
+          maxLen = dp[idx];
+          lastIdx = idx;
+      }
   }
 
   vector<int> lis;
