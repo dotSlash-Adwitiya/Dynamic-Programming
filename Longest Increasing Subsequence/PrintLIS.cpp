4bb744b044c912c407a1786b5fa846e5e1f2e72f
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-09-06 23:41:38 +0530

    DP.42 - Revised - Practice - 1

--- Longest Increasing Subsequence/PrintLIS.cpp
+++ Longest Increasing Subsequence/PrintLIS.cpp
@@ -32,3 +32,35 @@ vector<int> printingLongestIncreasingSubsequence(vector<int> arr, int n) {
   reverse(lis.begin(), lis.end());
   return lis;
 }
+
+// * Practice - 1
+vector<int> longestIncreasingSubsequence(int n, vector<int>& arr) {
+  vector<int> dp(n, 1), hash(n);
+  int lastIdx = 0, maxLen = 1;
+
+  for(int idx = 0; idx < n; idx++){
+    hash[idx] = idx;
+  
+    for(int prevIdx = 0; prevIdx <= idx-1; prevIdx++){
+      if(arr[prevIdx] < arr[idx] && 1 + dp[prevIdx] > dp[idx]){
+          dp[idx] = 1 + dp[prevIdx];
+          hash[idx] = prevIdx;
+      }
+    }
+    if(dp[idx] > maxLen){
+        maxLen = dp[idx];
+        lastIdx = idx; // * Store the last index of maximum dp[idx]
+    }
+}
+
+vector<int> lis;
+lis.push_back(arr[lastIdx]);
+
+while(lastIdx != hash[lastIdx]){
+    lastIdx = hash[lastIdx];
+    lis.push_back(arr[lastIdx]);
+}
+
+reverse(lis.begin(), lis.end());
+return lis;
+}
