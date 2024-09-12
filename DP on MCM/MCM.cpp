650cb8fc29c3884b338c1ab90caf5edc6cd4c422
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-09-12 23:10:05 +0530

    Tabulation Approach Added

--- DP on MCM/MCM.cpp
+++ DP on MCM/MCM.cpp
@@ -42,3 +42,22 @@ int matrixMultiplication(int N, int arr[]) {
   int j = N-1;
   return f(arr,i,j,dp);
 }
+
+// * TABULATION APPROACH
+int matrixMultiplication(vector<int>& arr, int N){
+  vector<vector<int>> dp(N,vector<int>(N,0));
+
+  for(int i = N-1; i >= 1; i--) {
+      for(int j = i + 1; j < N; j++) { // * j cannot be less than i
+          int mini = INT_MAX;
+          
+          // * PARTITIONING LOOP
+          for(int k = i; k<= j-1; k++){
+              int ans = dp[i][k]+ dp[k+1][j] + arr[i-1]*arr[k]*arr[j];
+              mini = min(mini,ans);
+          }
+          dp[i][j] = mini;
+      }
+  }
+  return dp[1][N-1];
+}
